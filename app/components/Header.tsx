bc749f5b505ffbc839856a7fe8ae1c072794d9f9
Brett Smith <jbsmith731@gmail.com>
2026-02-14 15:04:57 -0600

    feat: Add code formatting and styling improvements

---
+++ app/components/Header.tsx
@@ -0,0 +1,3 @@
+export function Header() {
+  return <header>Morgan Smith</header>;
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,4 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,5 +12,9 @@ export function loader({ context }: Route.LoaderArgs) {
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+  return (
+    <main>
+      <h1>{loaderData.message}</h1>
+    </main>
+  );
 }
